27875a4f6416198e23ea8f07e101fad037305580
Lee Santos <lee.m.santos@outlook.com>
2023-04-05 16:50:44 -0400

    Install body-parser and test post requests.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(PORT, () => {
